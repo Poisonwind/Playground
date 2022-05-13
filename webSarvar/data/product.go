664e0033d1d06402b6bab9bce036e0f54980fa9c
Pavel <pogorelchukpa@gmail.com>
2022-05-13 14:54:22 +0300

    method PUT finished

--- webSarvar/data/product.go
+++ webSarvar/data/product.go
@@ -2,31 +2,32 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
 type Product struct {
-	ID          int `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
 	Price       float32 `json:"price"`
-	SKU         string `json:"sku"`
-	CreatedOn   string `json:"-"`
-	UpdatedOn   string `json:"-"`
-	DeletedOn   string `json:"-"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
 }
 
 type Products []*Product
 
-func (p *Products) ToJSON (writer io.Writer) error {
+func (p *Products) ToJSON(writer io.Writer) error {
 
 	encoder := json.NewEncoder(writer)
 	return encoder.Encode(p)
 
 }
 
-func (p *Product) FromJSON (reader io.Reader) error {
+func (p *Product) FromJSON(reader io.Reader) error {
 
 	decoder := json.NewDecoder(reader)
 	return decoder.Decode(p)
@@ -37,15 +38,41 @@ func GetProducts() Products {
 	return ProductList
 }
 
-func GetNextID() (newId int) {
+//get id for new product
+func getNextID() (newId int) {
 	lastProduct := ProductList[len(ProductList)-1]
 	newId = lastProduct.ID + 1
 	return
 }
 
-func AddProduct (p *Product){
-	p.ID = GetNextID()
+//add new product to product list
+func AddProduct(p *Product) {
+	p.ID = getNextID()
 	ProductList = append(ProductList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	ProductList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for pos, prod := range ProductList {
+		if prod.ID == id {
+			return prod, pos, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
 }
 
 var ProductList = []*Product{
@@ -56,15 +83,15 @@ var ProductList = []*Product{
 		Price:       2.45,
 		SKU:         "abc123",
 		CreatedOn:   time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
 	},
 	&Product{
-		ID: 2,
-		Name: "Espresso",
+		ID:          2,
+		Name:        "Espresso",
 		Description: "Short and strong coffe without milk",
-		Price: 1.99,
-		SKU: "asd321",
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
+		Price:       1.99,
+		SKU:         "asd321",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
 	},
 }
--- webSarvar/handlers/products.go
+++ webSarvar/handlers/products.go
@@ -12,11 +12,11 @@ type Product struct {
 	log *log.Logger
 }
 
-func NewProduct (l *log.Logger) *Product {
+func NewProduct(l *log.Logger) *Product {
 	return &Product{l}
 }
 
-func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request){
+func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
@@ -29,23 +29,30 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request){
 	}
 
 	if r.Method == http.MethodPut {
+		p.log.Println("Use PUT Method", r.URL.Path)
+
 		exp := regexp.MustCompile(`/(\d+)`)
 		subStrings := exp.FindAllStringSubmatch(r.URL.Path, -1)
 
 		if len(subStrings) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			p.log.Println("Ivalid URL more then one id")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
 		}
 
-		if len(subStrings[0]) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		if len(subStrings[0]) != 2 {
+			p.log.Println("Ivalid URL more then one regexp group")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
 		}
 
 		idString := subStrings[0][1]
 		id, err := strconv.Atoi(idString)
 		if err != nil {
-			p.log.Println(err)
+			p.log.Println("Ivalid URL invalid to convert to number", idString)
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
 		}
 		p.log.Println("got id", id)
+		p.updateProducts(id, rw, r)
+		return
 	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -61,13 +68,13 @@ func (p *Product) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "unable to marshal json", http.StatusInternalServerError)
 	}
-	
+
 }
 
 func (p *Product) addProducts(rw http.ResponseWriter, r *http.Request) {
 
 	p.log.Println("Handle POST Products")
-	
+
 	prod := &data.Product{}
 	err := prod.FromJSON(r.Body)
 
@@ -77,4 +84,26 @@ func (p *Product) addProducts(rw http.ResponseWriter, r *http.Request) {
 
 	p.log.Printf("new prod: %#v", prod)
 	data.AddProduct(prod)
+}
+
+func (p *Product) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+
+	p.log.Println("Handle PUT Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.log.Printf("new prod: %#v", prod)
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "product not found", http.StatusInternalServerError)
+	}
 }
